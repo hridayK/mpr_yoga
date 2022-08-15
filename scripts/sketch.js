cc9c8d8c72b797485fa2d8b83927317c8e4ef8b5
Hriday Keswani <hridaykeswani@gmail.com>
2022-08-15 19:19:07 +0530

    implement nose tracking

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,11 +1,26 @@
 let video;
 let poseNet;
+
+let noseX = 40;
+let noseY = 40;
+
 function setup(){
     createCanvas(640, 480);
     video = createCapture(VIDEO);
     video.hide();
     poseNet = ml5.poseNet(video, modelReady);
+
+    poseNet.on('pose', (results) => {
+        poses = results;
+        console.log(poses);
+        noseX = poses[0].pose.keypoints[0].position.x;
+        noseY = poses[0].pose.keypoints[0].position.y;
+    });
+
+    
 }
+
+
 
 function modelReady(){
     console.log("model ready!!!!");
@@ -14,4 +29,7 @@ function modelReady(){
 function draw(){
     background(0);
     image(video, 0, 0, 640, 480);
+
+    fill(255,0,0);
+    ellipse(noseX,noseY,70);
 }
