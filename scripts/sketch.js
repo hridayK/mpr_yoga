7925880ee82109c061e88b1388ce0b72988f098d
Hriday Keswani <hridaykeswani@gmail.com>
2022-08-16 14:07:54 +0530

    implement body keypoint tracking

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,35 +1,39 @@
 let video;
 let poseNet;
-
-let noseX = 40;
-let noseY = 40;
+let pose;
+let X = 40;
+let Y = 40;
 
 function setup(){
     createCanvas(640, 480);
     video = createCapture(VIDEO);
     video.hide();
     poseNet = ml5.poseNet(video, modelReady);
-
-    poseNet.on('pose', (results) => {
-        poses = results;
-        console.log(poses);
-        noseX = poses[0].pose.keypoints[0].position.x;
-        noseY = poses[0].pose.keypoints[0].position.y;
-    });
-
-    
+    poseNet.on('pose', getPoses);
 }
 
 
+function getPoses(poses){
+    if(poses.length > 0){
+        pose = poses[0].pose.keypoints;
+    }
+}
 
 function modelReady(){
-    console.log("model ready!!!!");
+    console.log("model loaded");
 }
 
 function draw(){
     background(0);
     image(video, 0, 0, 640, 480);
-
-    fill(255,0,0);
-    ellipse(noseX,noseY,70);
+    if(pose){
+        // console.log(pose)
+        for(let i=0;i<pose.length;i++){
+            if(pose[i].score > 0.2){
+                fill(255);
+                ellipse(pose[i].position.x, pose[i].position.y, 20);
+            }
+        }
+        
+    }
 }
